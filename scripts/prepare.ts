95b7bee06631955a58204f88d2a2d7cfd73c9e0f
任天天 <ruattqwq@gmail.com>
2026-06-21 18:48:12 +0800

    change core cpu arch to v2

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -29,12 +29,12 @@ const MIHOMO_ALPHA_URL_PREFIX = `https://github.com/MetaCubeX/mihomo/releases/do
 let MIHOMO_ALPHA_VERSION: string
 
 const MIHOMO_ALPHA_MAP = {
-  'win32-x64': 'mihomo-windows-amd64',
+  'win32-x64': 'mihomo-windows-amd64-v2',
   'win32-ia32': 'mihomo-windows-386',
   'win32-arm64': 'mihomo-windows-arm64',
-  'darwin-x64': 'mihomo-darwin-amd64',
+  'darwin-x64': 'mihomo-darwin-amd64-v2',
   'darwin-arm64': 'mihomo-darwin-arm64',
-  'linux-x64': 'mihomo-linux-amd64',
+  'linux-x64': 'mihomo-linux-amd64-v2',
   'linux-arm64': 'mihomo-linux-arm64',
   'linux-loong64': 'mihomo-linux-loong64-abi2'
 }
@@ -61,12 +61,12 @@ const MIHOMO_URL_PREFIX = `https://github.com/MetaCubeX/mihomo/releases/download
 let MIHOMO_VERSION: string
 
 const MIHOMO_MAP = {
-  'win32-x64': 'mihomo-windows-amd64',
+  'win32-x64': 'mihomo-windows-amd64-v2',
   'win32-ia32': 'mihomo-windows-386',
   'win32-arm64': 'mihomo-windows-arm64',
-  'darwin-x64': 'mihomo-darwin-amd64',
+  'darwin-x64': 'mihomo-darwin-amd64-v2',
   'darwin-arm64': 'mihomo-darwin-arm64',
-  'linux-x64': 'mihomo-linux-amd64',
+  'linux-x64': 'mihomo-linux-amd64-v2',
   'linux-arm64': 'mihomo-linux-arm64',
   'linux-loong64': 'mihomo-linux-loong64-abi2'
 }
